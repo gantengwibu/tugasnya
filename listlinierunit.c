f207bfba187365e5bcb2ff5e3106c274cdc7830a
Wibu Parah <alnataraw@gmail.com>
2017-11-11 02:32:19 +0700

    hahahah

--- listlinierunit.c
+++ listlinierunit.c
@@ -13,20 +13,20 @@
 
 /* PROTOTYPE */
 /****************** TEST LIST KOSONG ******************/
-boolean IsEmpty (unitList L){
+boolean IsEmptyUnit (unitList L){
 /* Mengirim true jika list kosong */
 	return First(L)==Nil;
 	//timelimit();
 }
 /****************** PEMBUATAN LIST KOSONG ******************/
-void CreateEmpty (unitList *L){
+void CreateEmptyUnit (unitList *L){
 /* I.S. sembarang             */
 /* F.S. Terbentuk list kosong */
 	First(*L) = Nil;
 	//timelimit();
 }
 /****************** Manajemen Memori ******************/
-unitaddress Alokasi (unitinfotype X){
+unitaddress AlokasiUnit (unitinfotype X){
 /* Mengirimkan unitaddress hasil alokasi sebuah elemen */
 /* Jika alokasi berhasil, maka unitaddress tidak nil, dan misalnya */
 /* menghasilkan P, maka Info(P)=X, Next(P)=Nil */
@@ -43,7 +43,7 @@ unitaddress Alokasi (unitinfotype X){
 	}
 
 }
-void Dealokasi (unitaddress *P){
+void DealokasiUnit (unitaddress *P){
 /* I.S. P terdefinisi */
 /* F.S. P dikembalikan ke sistem */
 /* Melakukan dealokasi/pengembalian unitaddress P */
@@ -56,27 +56,27 @@ free(*P);
 
 /****************** PRIMITIF BERDASARKAN NILAI ******************/
 /*** PENAMBAHAN ELEMEN ***/
-void InsVFirst (unitList *L, unitinfotype X){
+void InsVFirstUnit (unitList *L, unitinfotype X){
 /* I.S. L mungkin kosong */
 /* F.S. Melakukan alokasi sebuah elemen dan */
 /* menambahkan elemen pertama dengan nilai X jika alokasi berhasil */
 //timelimit();	
-unitaddress p = Alokasi(X);
+unitaddress p = AlokasiUnit(X);
 	if (p!=Nil){
 		Next(p) = First(*L);
 		First(*L) = p;
 	}
 }
-void InsVLast (unitList *L, unitinfotype X){
+void InsVLastUnit (unitList *L, unitinfotype X){
 /* I.S. L mungkin kosong */
 /* F.S. Melakukan alokasi sebuah elemen dan */
 /* menambahkan elemen list di akhir: elemen terakhir yang baru */
 /* bernilai X jika alokasi berhasil. Jika alokasi gagal: I.S.= F.S. */
 //timelimit();
-	unitaddress masukan = Alokasi(X);
+	unitaddress masukan = AlokasiUnit(X);
 	if (masukan!=Nil){
 
-		if(IsEmpty(*L)){
+		if(IsEmptyUnit(*L)){
 			Next(masukan) = First(*L);
 			First(*L) = masukan;
 		} else{
@@ -95,7 +95,7 @@ void InsVLast (unitList *L, unitinfotype X){
 	}
 }
 /*** PENGHAPUSAN ELEMEN ***/
-void DelVFirst (unitList *L, unitinfotype *X){
+void DelVFirstUnit (unitList *L, unitinfotype *X){
 /* I.S. unitList L tidak kosong  */
 /* F.S. Elemen pertama list dihapus: nilai info disimpan pada X */
 /*      dan alamat elemen pertama di-dealokasi */
@@ -103,11 +103,11 @@ void DelVFirst (unitList *L, unitinfotype *X){
 	*X = Info(p);
 	First(*L) = Next(p);
 	Next(p) = Nil;
-	Dealokasi(&p);
+	DealokasiUnit(&p);
 
 
 }
-void DelVLast (unitList *L, unitinfotype *X){
+void DelVLastUnit (unitList *L, unitinfotype *X){
 /* I.S. list tidak kosong */
 /* F.S. Elemen terakhir list dihapus: nilai info disimpan pada X */
 /*      dan alamat elemen terakhir di-dealokasi */
@@ -127,18 +127,18 @@ void DelVLast (unitList *L, unitinfotype *X){
 		Next(prec) = Nil;
 
 	}
-	Dealokasi(&p);
+	DealokasiUnit(&p);
 }
 /****************** PRIMITIF BERDASARKAN ALAMAT ******************/
 /*** PENAMBAHAN ELEMEN BERDASARKAN ALAMAT ***/
-void InsertFirst (unitList *L, unitaddress P){
+void InsertFirstUnit (unitList *L, unitaddress P){
 /* I.S. Sembarang, P sudah dialokasi  */
 /* F.S. Menambahkan elemen ber-unitaddress P sebagai elemen pertama */
 //timelimit();	
 Next(P) = First(*L);
 	First(*L) = P;
 }
-void InsertAfter (unitList *L, unitaddress P, unitaddress Prec){
+void InsertAfterUnit (unitList *L, unitaddress P, unitaddress Prec){
 /* I.S. Prec pastilah elemen list dan bukan elemen terakhir, */
 /*      P sudah dialokasi  */
 /* F.S. Insert P sebagai elemen sesudah elemen beralamat Prec */
@@ -149,11 +149,11 @@ void InsertAfter (unitList *L, unitaddress P, unitaddress Prec){
 
 }
 
-void InsertLast (unitList *L, unitaddress P){
+void InsertLastUnit (unitList *L, unitaddress P){
 /* I.S. Sembarang, P sudah dialokasi  */
 /* F.S. P ditambahkan sebagai elemen terakhir yang baru */
 //timelimit();	
-if (IsEmpty(*L)){
+if (IsEmptyUnit(*L)){
 		First(*L) = P;
 	} else{
 		unitaddress a = First(*L);
@@ -166,7 +166,7 @@ if (IsEmpty(*L)){
 
 }
 /*** PENGHAPUSAN SEBUAH ELEMEN ***/
-void DelFirst (unitList *L, unitaddress *P){
+void DelFirstUnit (unitList *L, unitaddress *P){
 /* I.S. unitList tidak kosong */
 /* F.S. P adalah alamat elemen pertama list sebelum penghapusan */
 /*      Elemen list berkurang satu (mungkin menjadi kosong) */
@@ -180,7 +180,7 @@ void DelFirst (unitList *L, unitaddress *P){
 
 }
 
-void DelLast (unitList *L, unitaddress *P){
+void DelLastUnit (unitList *L, unitaddress *P){
 /* I.S. unitList tidak kosong */
 /* F.S. P adalah alamat elemen terakhir list sebelum penghapusan  */
 /*      Elemen list berkurang satu (mungkin menjadi kosong) */
@@ -209,7 +209,7 @@ void DelLast (unitList *L, unitaddress *P){
 	}
 
 }
-void DelAfter (unitList *L, unitaddress *Pdel, unitaddress Prec){
+void DelAfterUnit (unitList *L, unitaddress *Pdel, unitaddress Prec){
 /* I.S. unitList tidak kosong. Prec adalah anggota list  */
 /* F.S. Menghapus Next(Prec): */
 /*      Pdel adalah alamat elemen list yang dihapus  */
@@ -220,11 +220,11 @@ void DelAfter (unitList *L, unitaddress *Pdel, unitaddress Prec){
 } 
 /****************** PROSES SEMUA ELEMEN LIST ******************/
 
-int NbElmt (unitList L){
+int NbElmtUnit (unitList L){
 	//timelimit();
 /* Mengirimkan banyaknya elemen list; mengirimkan 0 jika list kosong */
 	unitaddress p = First(L);
-	if (IsEmpty(L)){
+	if (IsEmptyUnit(L)){
 		return 0 ;
 
 	} else{
@@ -239,7 +239,7 @@ int NbElmt (unitList L){
 }
 
 /****************** PROSES TERHADAP LIST ******************/
-void Konkat1 (unitList *L1, unitList *L2, unitList *L3){
+void Konkat1Unit (unitList *L1, unitList *L2, unitList *L3){
 	//timelimit();
 /* I.S. L1 dan L2 sembarang */
 /* F.S. L1 dan L2 kosong, L3 adalah hasil konkatenasi L1 & L2 */
@@ -247,8 +247,8 @@ void Konkat1 (unitList *L1, unitList *L2, unitList *L3){
 /* menghasilkan L3 yang baru (dengan elemen list L1 dan L2) */
 /* dan L1 serta L2 menjadi list kosong.*/
 /* Tidak ada alokasi/dealokasi pada prosedur ini */
-	CreateEmpty(L3);
-	if(IsEmpty(*L1)){
+	CreateEmptyUnit(L3);
+	if(IsEmptyUnit(*L1)){
 		First(*L3)=First(*L2);
 	} else{
 
@@ -264,8 +264,8 @@ void Konkat1 (unitList *L1, unitList *L2, unitList *L3){
 
 	}
 
-	CreateEmpty(L1);
-	CreateEmpty(L2);
+	CreateEmptyUnit(L1);
+	CreateEmptyUnit(L2);
 }
 
 
@@ -311,7 +311,7 @@ Info(b) = temp;
 
 
 
-boolean FSearch (unitList L, unitaddress P){
+boolean FSearchUnit (unitList L, unitaddress P){
 	
 /* Mencari apakah ada elemen list yang beralamat P */
 /* Mengirimkan true jika ada, false jika tidak ada */
@@ -336,22 +336,22 @@ boolean FSearch (unitList L, unitaddress P){
 
 
 /****************** PROSES TERHADAP LIST ******************/
-void DelAll (unitList *L){
+void DelAllUnit (unitList *L){
 /* Delete semua elemen list dan alamat elemen di-dealokasi */
 	unitaddress p =First(*L);
 	First(*L)=Nil;
 	while(p!=Nil){
-		Dealokasi(&p);
+		DealokasiUnit(&p);
 		p=Next(p);
 	}
 
 }
-void InversList (unitList *L){
+void InversListUnit (unitList *L){
 /* I.S. sembarang. */
 /* F.S. elemen list dibalik : */
 /* Elemen terakhir menjadi elemen pertama, dan seterusnya. */
 /* Membalik elemen list, tanpa melakukan alokasi/dealokasi. */
-	if (!IsEmpty(*L)){
+	if (!IsEmptyUnit(*L)){
 		unitaddress p =First(*L);
 		if (Next(p)!=Nil){
 			unitaddress prec = p;
@@ -370,14 +370,14 @@ void InversList (unitList *L){
 	}
 
 }
-unitList FInversList (unitList L){
+unitList FInversListUnit (unitList L){
 /* Mengirimkan list baru, hasil invers dari L */
-/* dengan menyalin semua elemn list. Alokasi mungkin gagal. */
+/* dengan menyalin semua elemn list. AlokasiUnit mungkin gagal. */
 /* Jika alokasi gagal, hasilnya list kosong */
 /* dan semua elemen yang terlanjur di-alokasi, harus didealokasi */
-	if(!IsEmpty(L)){
+	if(!IsEmptyUnit(L)){
 		unitaddress p =First(L);
-		unitaddress allocf = Alokasi(Info(p));
+		unitaddress allocf = AlokasiUnit(Info(p));
 		unitList baru ;
 		First(baru) = allocf;
 		
@@ -385,17 +385,17 @@ unitList FInversList (unitList L){
 			p = Next(p);
 			boolean gagal = false;
 			while((p!=Nil)&& (gagal ==false)){
-				unitaddress temp = Alokasi(Info(p));
+				unitaddress temp = AlokasiUnit(Info(p));
 				if (temp==Nil){
 					gagal =true;
 				} else{
-					InsertFirst(&baru,temp);
+					InsertFirstUnit(&baru,temp);
 					}
 				p=Next(p);	
 			}
 			
 			if (gagal){
-				DelAll(&baru);
+				DelAllUnit(&baru);
 			}
 			
 			return baru;
@@ -406,21 +406,21 @@ unitList FInversList (unitList L){
 
 
 }
-void CopyList (unitList *L1, unitList *L2){
+void CopyListUnit (unitList *L1, unitList *L2){
 /* I.S. L1 sembarang. F.S. L2=L1 */
 /* L1 dan L2 "menunjuk" kepada list yang sama.*/
 /* Tidak ada alokasi/dealokasi elemen */
 	First(*L2) = First(*L1);
 
 }
-unitList FCopyList (unitList L){
+unitList FCopyListUnit (unitList L){
 /* Mengirimkan list yang merupakan salinan L */
 /* dengan melakukan alokasi. */
 /* Jika ada alokasi gagal, hasilnya list kosong dan */
 /* semua elemen yang terlanjur di-alokasi, harus didealokasi */
-	if(!IsEmpty(L)){
+	if(!IsEmptyUnit(L)){
 		unitaddress p =First(L);
-		unitaddress allocf = Alokasi(Info(p));
+		unitaddress allocf = AlokasiUnit(Info(p));
 		unitList baru ;
 		First(baru) = allocf;
 		
@@ -428,17 +428,17 @@ unitList FCopyList (unitList L){
 			p = Next(p);
 			boolean gagal = false;
 			while((p!=Nil)&& (gagal ==false)){
-				unitaddress temp = Alokasi(Info(p));
+				unitaddress temp = AlokasiUnit(Info(p));
 				if (temp==Nil){
 					gagal =true;
 				} else{
-					InsertLast(&baru,temp);
+					InsertLastUnit(&baru,temp);
 					}
 				p=Next(p);	
 			}
 			
 			if (gagal){
-				DelAll(&baru);
+				DelAllUnit(&baru);
 			}
 			
 			return baru;
@@ -449,16 +449,16 @@ unitList FCopyList (unitList L){
 
 }
 
-void CpAlokList (unitList Lin, unitList *Lout){
+void CpAlokListUnit (unitList Lin, unitList *Lout){
 /* I.S. Lin sembarang. */
 /* F.S. Jika semua alokasi berhasil,maka Lout berisi hasil copy Lin */
 /* Jika ada alokasi yang gagal, maka Lout=Nil dan semua elemen yang terlanjur dialokasi, didealokasi */
 /* dengan melakukan alokasi elemen. */
 /* Lout adalah list kosong jika ada alokasi elemen yang gagal */
 
-	*Lout = FCopyList(Lin);
+	*Lout = FCopyListUnit(Lin);
 }
-void Konkat (unitList L1, unitList L2, unitList * L3){
+void KonkatUnit (unitList L1, unitList L2, unitList * L3){
 /* I.S. L1 dan L2 sembarang */
 /* F.S. L1 dan L2 tetap, L3 adalah hasil konkatenasi L1 & L2 */
 /* Jika semua alokasi berhasil, maka L3 adalah hasil konkatenasi*/
@@ -469,49 +469,49 @@ void Konkat (unitList L1, unitList L2, unitList * L3){
 /* Jika ada alokasi yang gagal, maka L3 harus bernilai Nil*/
 /* dan semua elemen yang pernah dialokasi didealokasi */
 	unitList b1,b2;
-	CreateEmpty(&b1);
-	CreateEmpty(&b2);
+	CreateEmptyUnit(&b1);
+	CreateEmptyUnit(&b2);
 	
-	b1=FCopyList(L1);
-	b2=FCopyList(L2);
-	Konkat1(&b1,&b2,L3);
+	b1=FCopyListUnit(L1);
+	b2=FCopyListUnit(L2);
+	Konkat1Unit(&b1,&b2,L3);
 
 }
 
 
-void PecahList (unitList *L1, unitList *L2, unitList L){
+void PecahListUnit (unitList *L1, unitList *L2, unitList L){
 /* I.S. L mungkin kosong */
 /* F.S. Berdasarkan L, dibentuk dua buah list L1 dan L2 */
 /* L tidak berubah: untuk membentuk L1 dan L2 harus alokasi */
 /* L1 berisi separuh elemen L dan L2 berisi sisa elemen L */
-/* Jika elemen L ganjil, maka separuh adalah NbElmt(L) div 2 */
-		CreateEmpty(L1);
-		CreateEmpty(L2);
+/* Jika elemen L ganjil, maka separuh adalah NbElmtUnit(L) div 2 */
+		CreateEmptyUnit(L1);
+		CreateEmptyUnit(L2);
 	
-	if (IsEmpty(L)){
-		CreateEmpty(L1);
-		CreateEmpty(L2);
+	if (IsEmptyUnit(L)){
+		CreateEmptyUnit(L1);
+		CreateEmptyUnit(L2);
 	} else{
-			int jumlah= NbElmt(L);
+			int jumlah= NbElmtUnit(L);
 			
 			int j1 = jumlah /2;			
 			unitaddress p = First(L);
 			if (j1 == 0 ){
-				CreateEmpty(L1);
+				CreateEmptyUnit(L1);
 			} else{
 				int i=1;
 				for(i=1;i<=j1;i++){
-					unitaddress temp = Alokasi(Info(p));
+					unitaddress temp = AlokasiUnit(Info(p));
 					
-					InsertLast(L1,temp);
+					InsertLastUnit(L1,temp);
 					p=Next(p);
 				}
 			}
 			int i;
 			for(i=j1+1;i<=jumlah;i++){
-				unitaddress temp = Alokasi(Info(p));
+				unitaddress temp = AlokasiUnit(Info(p));
 					
-				InsertLast(L2,temp);
+				InsertLastUnit(L2,temp);
 				p=Next(p);
 			}
 			
--- listlinierunit.h
+++ listlinierunit.h
@@ -30,31 +30,31 @@ typedef struct {
 
 /* PROTOTYPE */
 /****************** TEST LIST KOSONG ******************/
-boolean IsEmpty (unitList L);
+boolean IsEmptyUnit (unitList L);
 /* Mengirim true jika list kosong */
 
 /****************** PEMBUATAN LIST KOSONG ******************/
-void CreateEmpty (unitList *L);
+void CreateEmptyUnit (unitList *L);
 /* I.S. sembarang             */
 /* F.S. Terbentuk list kosong */
 
 /****************** Manajemen Memori ******************/
-unitaddress Alokasi (unitinfotype X);
+unitaddress AlokasiUnit (unitinfotype X);
 /* Mengirimkan unitaddress hasil alokasi sebuah elemen */
 /* Jika alokasi berhasil, maka unitaddress tidak nil, dan misalnya */
 /* menghasilkan P, maka info(P)=X, Next(P)=Nil */
 /* Jika alokasi gagal, mengirimkan Nil */
-void Dealokasi (unitaddress *P);
+void DealokasiUnit (unitaddress *P);
 /* I.S. P terdefinisi */
 /* F.S. P dikembalikan ke sistem */
 /* Melakukan dealokasi/pengembalian unitaddress P */
 
 /****************** PENCARIAN SEBUAH ELEMEN LIST ******************/
 
-boolean FSearch (unitList L, unitaddress P);
+boolean FSearchUnit (unitList L, unitaddress P);
 /* Mencari apakah ada elemen list yang beralamat P */
 /* Mengirimkan true jika ada, false jika tidak ada */
-unitaddress SearchPrec (unitList L, unitinfotype X);
+unitaddress SearchPrecUnit (unitList L, unitinfotype X);
 /* Mengirimkan unitaddress elemen sebelum elemen yang nilainya=X */
 /* Mencari apakah ada elemen list dengan Info(P)=X */
 /* Jika ada, mengirimkan unitaddress Prec, dengan Next(Prec)=P dan Info(P)=X. */
@@ -65,99 +65,99 @@ unitaddress SearchPrec (unitList L, unitinfotype X);
 
 /****************** PRIMITIF BERDASARKAN NILAI ******************/
 /*** PENAMBAHAN ELEMEN ***/
-void InsVFirst (unitList *L, unitinfotype X);
+void InsVFirstUnit (unitList *L, unitinfotype X);
 /* I.S. L mungkin kosong */
 /* F.S. Melakukan alokasi sebuah elemen dan */
 /* menambahkan elemen pertama dengan nilai X jika alokasi berhasil */
-void InsVLast (unitList *L, unitinfotype X);
+void InsVLastUnit (unitList *L, unitinfotype X);
 /* I.S. L mungkin kosong */
 /* F.S. Melakukan alokasi sebuah elemen dan */
 /* menambahkan elemen list di akhir: elemen terakhir yang baru */
 /* bernilai X jika alokasi berhasil. Jika alokasi gagal: I.S.= F.S. */
 
 /*** PENGHAPUSAN ELEMEN ***/
-void DelVFirst (unitList *L, unitinfotype *X);
+void DelVFirstUnit (unitList *L, unitinfotype *X);
 /* I.S. unitList L tidak kosong  */
 /* F.S. Elemen pertama list dihapus: nilai info disimpan pada X */
 /*      dan alamat elemen pertama di-dealokasi */
-void DelVLast (unitList *L, unitinfotype *X);
+void DelVLastUnit (unitList *L, unitinfotype *X);
 /* I.S. list tidak kosong */
 /* F.S. Elemen terakhir list dihapus: nilai info disimpan pada X */
 /*      dan alamat elemen terakhir di-dealokasi */
 
 /****************** PRIMITIF BERDASARKAN ALAMAT ******************/
 /*** PENAMBAHAN ELEMEN BERDASARKAN ALAMAT ***/
-void InsertFirst (unitList *L, unitaddress P);
+void InsertFirstUnit (unitList *L, unitaddress P);
 /* I.S. Sembarang, P sudah dialokasi  */
 /* F.S. Menambahkan elemen ber-unitaddress P sebagai elemen pertama */
-void InsertAfter (unitList *L, unitaddress P, unitaddress Prec);
+void InsertAfterUnit (unitList *L, unitaddress P, unitaddress Prec);
 /* I.S. Prec pastilah elemen list dan bukan elemen terakhir, */
 /*      P sudah dialokasi  */
 /* F.S. Insert P sebagai elemen sesudah elemen beralamat Prec */
-void InsertLast (unitList *L, unitaddress P);
+void InsertLastUnit (unitList *L, unitaddress P);
 /* I.S. Sembarang, P sudah dialokasi  */
 /* F.S. P ditambahkan sebagai elemen terakhir yang baru */
 
 /*** PENGHAPUSAN SEBUAH ELEMEN ***/
-void DelFirst (unitList *L, unitaddress *P);
+void DelFirstUnit (unitList *L, unitaddress *P);
 /* I.S. unitList tidak kosong */
 /* F.S. P adalah alamat elemen pertama list sebelum penghapusan */
 /*      Elemen list berkurang satu (mungkin menjadi kosong) */
 /* First element yg baru adalah suksesor elemen pertama yang lama */
 
-void DelLast (unitList *L, unitaddress *P);
+void DelLastUnit (unitList *L, unitaddress *P);
 /* I.S. unitList tidak kosong */
 /* F.S. P adalah alamat elemen terakhir list sebelum penghapusan  */
 /*      Elemen list berkurang satu (mungkin menjadi kosong) */
 /* Last element baru adalah predesesor elemen terakhir yg lama, */
 /* jika ada */
-void DelAfter (unitList *L, unitaddress *Pdel, unitaddress Prec);
+void DelAfterUnit (unitList *L, unitaddress *Pdel, unitaddress Prec);
 /* I.S. unitList tidak kosong. Prec adalah anggota list  */
 /* F.S. Menghapus Next(Prec): */
 /*      Pdel adalah alamat elemen list yang dihapus  */
 
 /****************** PROSES SEMUA ELEMEN LIST ******************/
 
-int NbElmt (unitList L);
+int NbElmtUnit (unitList L);
 /* Mengirimkan banyaknya elemen list; mengirimkan 0 jika list kosong */
 
 
 
 /****************** PROSES TERHADAP LIST ******************/
-void DelAll (unitList *L);
+void DelAllUnit (unitList *L);
 /* Delete semua elemen list dan alamat elemen di-dealokasi */
 
-void InversList (unitList *L);
+void InversListUnit (unitList *L);
 /* I.S. sembarang. */
 /* F.S. elemen list dibalik : */
 /* Elemen terakhir menjadi elemen pertama, dan seterusnya. */
 /* Membalik elemen list, tanpa melakukan alokasi/dealokasi. */
 
-unitList FInversList (unitList L);
+unitList FInversListUnit (unitList L);
 /* Mengirimkan list baru, hasil invers dari L */
-/* dengan menyalin semua elemn list. Alokasi mungkin gagal. */
+/* dengan menyalin semua elemn list. AlokasiUnit mungkin gagal. */
 /* Jika alokasi gagal, hasilnya list kosong */
 /* dan semua elemen yang terlanjur di-alokasi, harus didealokasi */
 
-void CopyList (unitList *L1, unitList *L2);
+void CopyListUnit (unitList *L1, unitList *L2);
 /* I.S. L1 sembarang. F.S. L2=L1 */
 /* L1 dan L2 "menunjuk" kepada list yang sama.*/
 /* Tidak ada alokasi/dealokasi elemen */
 
-unitList FCopyList (unitList L);
+unitList FCopyListUnit (unitList L);
 /* Mengirimkan list yang merupakan salinan L */
 /* dengan melakukan alokasi. */
 /* Jika ada alokasi gagal, hasilnya list kosong dan */
 /* semua elemen yang terlanjur di-alokasi, harus didealokasi */
 
-void CpAlokList (unitList Lin, unitList *Lout);
+void CpAlokListUnit (unitList Lin, unitList *Lout);
 /* I.S. Lin sembarang. */
 /* F.S. Jika semua alokasi berhasil,maka Lout berisi hasil copy Lin */
 /* Jika ada alokasi yang gagal, maka Lout=Nil dan semua elemen yang terlanjur dialokasi, didealokasi */
 /* dengan melakukan alokasi elemen. */
 /* Lout adalah list kosong jika ada alokasi elemen yang gagal */
 
-void Konkat (unitList L1, unitList L2, unitList * L3);
+void KonkatUnit (unitList L1, unitList L2, unitList * L3);
 /* I.S. L1 dan L2 sembarang */
 /* F.S. L1 dan L2 tetap, L3 adalah hasil konkatenasi L1 & L2 */
 /* Jika semua alokasi berhasil, maka L3 adalah hasil konkatenasi*/
@@ -168,7 +168,7 @@ void Konkat (unitList L1, unitList L2, unitList * L3);
 /* Jika ada alokasi yang gagal, maka L3 harus bernilai Nil*/
 /* dan semua elemen yang pernah dialokasi didealokasi */
 
-void Konkat1 (unitList *L1, unitList *L2, unitList *L3);
+void Konkat1Unit (unitList *L1, unitList *L2, unitList *L3);
 /* I.S. L1 dan L2 sembarang */
 /* F.S. L1 dan L2 kosong, L3 adalah hasil konkatenasi L1 & L2 */
 /* Konkatenasi dua buah list : L1 dan L2    */
@@ -176,7 +176,7 @@ void Konkat1 (unitList *L1, unitList *L2, unitList *L3);
 /* dan L1 serta L2 menjadi list kosong.*/
 /* Tidak ada alokasi/dealokasi pada prosedur ini */
 
-void PecahList (unitList *L1, unitList *L2, unitList L);
+void PecahListUnit (unitList *L1, unitList *L2, unitList L);
 /* I.S. L mungkin kosong */
 /* F.S. Berdasarkan L, dibentuk dua buah list L1 dan L2 */
 /* L tidak berubah: untuk membentuk L1 dan L2 harus alokasi */
